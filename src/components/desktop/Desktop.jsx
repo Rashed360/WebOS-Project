5d74f5a3d43d2bcdc7a96b8857634fb847a2f383
Rashed Ahmed <rashed.ahmed@email.com>
2022-01-31 23:44:18 +0600

    desktop apps

--- src/components/desktop/Desktop.jsx
+++ src/components/desktop/Desktop.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import DesktopAPP from './desktopapp/DesktopAPP'
 
 const Desktop = () => {
-  return <div className='desktop'>Desktop</div>
+  return (
+    <div className='desktop'>
+      <div className="desktop-content">
+        <DesktopAPP />
+        <DesktopAPP />
+        <DesktopAPP />
+        <DesktopAPP />
+        <DesktopAPP />
+        <DesktopAPP />
+        <DesktopAPP />
+        <DesktopAPP />
+        <DesktopAPP />
+      </div>
+    </div>
+  )
 }
 
 export default Desktop
---
+++ src/components/desktop/desktopapp/DesktopAPP.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DesktopAPP = () => {
+  return (
+    <div className="desktop-app">
+        <div className="icon"></div>
+        <div className="text">Files</div>
+    </div>
+  )
+}
+
+export default DesktopAPP
